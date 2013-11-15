c59f0404a239e693afac963f0feb1645c60a751f
Donn Felker <donn@donnfelker.com>
2013-11-15 08:46:03 -0800

    Adding comments to UserAgentProvider

--- app/src/main/java/com/donnfelker/android/bootstrap/core/UserAgentProvider.java
+++ app/src/main/java/com/donnfelker/android/bootstrap/core/UserAgentProvider.java
@@ -16,6 +16,26 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 
 
+/**
+ * Class that builds a User-Agent that is set on all HTTP calls.
+ *
+ * The user agent will change depending on the version of Android that
+ * the user is running, the device their running and the version of the
+ * app that they're running. This will allow your remote API to perform
+ * User-Agent inspection to provide different logic routes or analytics
+ * based upon the User-Agent.
+ *
+ * Example of what is generated when running the Genymotion Nexus 4 Emulator:
+ *
+ *      Android Bootstrap/1.0 (Android 4.2.2; Genymotion Vbox86p / Generic Galaxy Nexus - 4.2.2 - API 17 - 720x1280; )[preload=false;locale=en_US;clientidbase=]
+ *
+ * The value "preload" means that the app has been preloaded by the manufacturer.
+ * Instances of when this might happen is if you partner with a telecom company
+ * to ship your app with their new device.
+ *
+ * If clientidbase is available you "should" be getting the telecom that is operating
+ * the device. This is not reliable, but is still useful. 
+ */
 public class UserAgentProvider implements Provider<String> {
 
     private static final String APP_NAME = "Android Bootstrap";
